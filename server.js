d8408eabd2ab353a9d7b1e2ed25e25f09532d82f
HakimElkabouss <elkabousshakim@hotmail.com>
2019-07-24 17:39:35 +0200

    connexion mysql

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 var nodemailer = require('nodemailer');
 var configGmail = require('./configGmail');
 var Routers = require('./Routers').router;
@@ -11,9 +12,55 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', Routers);
 
+var connection = mysql.createConnection({
+
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'database_development_sequelize'
+
+});
+
+connection.connect();
+
+app.set('view engine', 'ejs');
 
 
+// show users
+app.get('/admin', function(req, res, next){
+  connection.query('SELECT * FROM users', function(err, rs){
+    res.render('admin', { user : rs });
+  })
+})
 
+// Delete a user
+app.get('/delete', function(req, res){
+  connection.query('DELETE FROM users WHERE id = ?', req.query.id, function(err, rs){
+    res.redirect('/admin');
+  })
+})
+
+
+// delete a user 
+// app.delete('/admin/:id',function(req,res){
+//   connection.query('DELETE FROM users WHERE id = ?', [req.params.id], function(error, data){
+//     res.send('Deleted Successfully');
+//   })
+// })
+
+
+// add a publication
+app.post('/welcome', function(req, res){
+   
+  connection.query("INSERT INTO messages SET ?", req.body, function(err, data){
+      if(err) throw err;
+
+      console.log("1 publication inserted");
+  });
+
+});
+
+// Newsletter
 app.post('/newsletter', function(req, res, next) {
   var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -22,12 +69,17 @@ app.post('/newsletter', function(req, res, next) {
       pass: configGmail.gmail.GmailKey
     }
   });
-  
+  var mailbody = `
+        <h3> Hi Guest !</h3>
+        <p>It is a great pleasure to us to choose our application and put your trust in us</p>
+        <p>now your mail is registered in our newsletter box</p>
+        <p>be ready you will receive all our update, see you soon ;)</p>
+        `
   var mailOptions = {
-    from: "Rocket",
+    from: "<noreply>",
     to: req.body.text ,
     subject: 'Welcome to Rocket',
-    text: "It is a great pleasure to us to choose our application and put your trust in us, now your mail is registered in our newsletter box, be ready you will receive all our update, see you soon ;)"
+    html: mailbody
 };
   
 transporter.sendMail(mailOptions, function(error, info){
@@ -36,13 +88,9 @@ if(error){
 }
 console.log('Message sent: ' + info.response);
 });
-
+  res.redirect('/welcome');
   transporter.close();
 });
-
-
-
-
 
 
 
